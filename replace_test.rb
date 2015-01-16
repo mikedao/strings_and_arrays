1cbb58abf99712cf43bdb3ab6dc09c9581c372a4
Will Faurot <wfaurot@gmail.com>
2015-01-15 23:43:02 -0700

    Add skips to all but first tests
    This follows exercism's style, and fits with the idea of starting
    small and gradually making more complex tests pass.

--- replace_test.rb
+++ replace_test.rb
@@ -10,16 +10,19 @@ class ReplaceTest < Minitest::Test
   end
 
   def test_it_replaces_multiple_spaces
+    skip
     result = replace("Foo Bar Baz")
     assert_equal "Foo%20Bar%20Baz", result
   end
 
   def test_it_does_not_replace_trailing_spaces_but_removes_them
+    skip
     result = replace("FooBar  ")
     assert_equal "FooBar", result
   end
 
   def test_it_replaces_spaces_and_removes_trailing_ones
+    skip
     result = replace("Foo Bar Baz     ")
     assert_equal "Foo%20Bar%20Baz", result
   end
